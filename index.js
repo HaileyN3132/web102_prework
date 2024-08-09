52af0b5c9f8d02f778b47167d55c7c441dca7828
Hailey <ngohailey02@gmail.com>
2024-08-09 14:43:18 -0400

    modified addGamesToPage() and called it

--- index.js
+++ index.js
@@ -27,29 +27,37 @@ const gamesContainer = document.getElementById("games-container");
 
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
-
-    // loop over each item in the data
-
-
-        // create a new div element, which will become the game card
-
-
-        // add the class game-card to the list
-
-
-        // set the inner HTML using a template literal to display some info 
-        // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
 
+        // loop over each item in the data
+        games.forEach((game) =>{
+            // create a new div element, which will become the game card
+            const newDiv = document.createElement("div");
 
-        // append the game to the games-container
+            // add the class game-card to the list
+            newDiv.classList.add('game-card');
+
+            // set the inner HTML using a template literal to display some info 
+            // about each game
+            newDiv.innerHTML = `
+                <img class="game-img" src="${game.img}">
+                <p>Name: ${game.name}</p>
+                <p>Description: ${game.description}</p>
+                <p>Pledged: ${game.pledged}</p>
+                <p>Goal: ${game.goal}</p>
+                <p>Backers: ${game.backers}</p>
+            `;
+            // append the game to the games-container
+            document.getElementById("games-container").appendChild(newDiv);
+
+        });
 
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
